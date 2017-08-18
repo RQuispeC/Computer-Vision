7c681b1d9c2f1ed657332b0be3f32f0ef4f8ddce
Darwin Tito Concha <ra192618@santos.lab.ic.unicamp.br>
2017-08-18 20:21:34 -0300

    source for question 3-gaussian pyramid

--- HW1/src/pyramide.py
+++ HW1/src/pyramide.py
@@ -10,14 +10,44 @@ class gaussian_pyramid:
         self.levels = levels
         self.kernel = utils.create_kernel(kernel_a)
     
-    def interpolation(self,x,y,v):
-        return 0
-        
-    def up_sample(self, image, size):
+    def interpolation(self,x,y,v, interp = 'bilinear'):
+        if interp=='bilinear':  
+            if (x[0]==x[1]==x[2]==x[3]) or (y[0]==y[1]==y[2]==y[3]):
+                return v[0]
+            r1=(((x[1]-x[4])/(x[1]-x[0]))*v[2]+(((x[4]-x[0])/(x[1]-x[0])))*v[3])
+            r2=(((x[1]-x[4])/(x[1]-x[0]))*v[0]+(((x[4]-x[0])/(x[1]-x[0])))*v[1])
+            p=(((y[1]-y[4])/(y[0]-y[2]))*r1+(((y[4]-y[2])/(y[0]-y[2])))*r2)
+            return p
         return 1
+
+    def up_sample(self, image, size, interp = 'bilinear'):
+        new_image=np.empty(size)
+
+        for i in range(size[0]):
+            for j in range(size[1]):
+                if(interp == 'bilinear'):
+                    i_min = min(i//2, image.shape[0]-1)
+                    j_min = min(j//2, image.shape[1]-1)
+                    i_lim = min(i_min +1, image.shape[0]-1)
+                    j_lim = min(j_min +1, image.shape[1]-1)
+                    y = [i_min, i_min, i_lim, i_lim, (i_min + i_lim)/2.0]
+                    x = [j_min, j_lim, j_min, j_lim, (j_min + j_lim)/2.0]
+                    z = [image[i_min, j_min], image[i_min, j_lim], image[i_lim, j_min], image[i_lim, j_lim]]
+                    new_image[i, j] = self.interpolation(x, y, z, interp)
+        return new_image
     
     def down(self, level): #upsample
-		return 1
+        axis_x=self.pyramid[level].shape[0] * 2
+        axis_y=self.pyramid[level].shape[1] * 2
+        if len(self.img.shape) == 3 :
+            new_image = np.empty((axis_x,axis_y, 3))
+            new_image[:, :, 0] = self.up_sample(self.pyramid[level][:, :, 0],(axis_x,axis_y))
+            new_image[:, :, 1] = self.up_sample(self.pyramid[level][:, :, 1],(axis_x,axis_y))
+            new_image[:, :, 2] = self.up_sample(self.pyramid[level][:, :, 2],(axis_x,axis_y))
+            return new_image
+        else:
+            new_image = self.up_sample(self.pyramid[level],(axis_x,axis_y))
+            return new_image
         
     def down_sample(self, image,size):
         new_image=np.empty(size)
@@ -63,4 +93,4 @@ if __name__ == "__main__":
     pyramide=gaussian_pyramid(image,7)
     pyramide.build()
     pyramide.show()
-    pyramide.down(0)
+    cv2.imwrite("juan.jpg",pyramide.down(0))
