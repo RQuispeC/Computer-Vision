de69b827ab00b49407fd55e4c537391cb367bf0b
darwinTC <d192618@g.unicamp.br>
2017-10-05 22:30:38 -0300

    update my-output and structure from motion

--- HW3/create-ziped.sh
+++ HW3/create-ziped.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ziped_dir="p1-192617-192618"
+ziped_dir="p3-192617-192618"
 
 rm -rf $ziped_dir
 rm -f "$ziped_dir.zip"
@@ -8,8 +8,9 @@ rm -f "$ziped_dir.zip"
 mkdir $ziped_dir
 mkdir "$ziped_dir/src"
 cp -R src/*.py "$ziped_dir/src"
-cp -R input $ziped_dir
 mkdir "$ziped_dir/output"
+mkdir "$ziped_dir/input"
+cp -R my-output "$ziped_dir"
 cp Makefile $ziped_dir
 cp report/report.pdf $ziped_dir
 zip -r "$ziped_dir.zip" $ziped_dir
--- HW3/src/structure_from_motion.py
+++ HW3/src/structure_from_motion.py
@@ -2,6 +2,9 @@ import cv2
 import numpy as np
 from random import randint
 
+'''
+    Load Video
+'''
 def load_video(file_name = 'input/p2-1-1.mp4', fps = 30, original_fps = 30):
     #kernel = np.ones((5,5),np.float32)/25
     if fps > original_fps or fps < 1:
@@ -22,7 +25,9 @@ def load_video(file_name = 'input/p2-1-1.mp4', fps = 30, original_fps = 30):
         success, img = vidcap.read()
     return video
 
-#compute U, V
+'''
+    Compute U, V
+'''
 def interpolation(x, y, img, isfirst):
     if isfirst:
         return img[int(x)][int(y)]
@@ -45,12 +50,14 @@ def derivate(img, direct='X'):
     else :
         return cv2.filter2D(img, -1, kernelY)
 
-#find keypoints 
-def interest_point(frame, method='shiTomosi', harris_block_sz = 2, harris_aperture_sz = 3, harris_k = 0.04, prunning_thresh=0.15):
+'''
+    Find keypoints
+'''
+def interest_point(frame, method='shiTomosi', harris_block_sz = 2, harris_aperture_sz = 3, harris_k = 0.04, prunning_thresh=0.05):
     kpt=[]
     if method == 'shiTomosi':
         # params for ShiTomasi corner detection
-        feature_params = dict( maxCorners = 1000, qualityLevel = 0.3, minDistance = 7, blockSize = 7 )
+        feature_params = dict( maxCorners = 10000, qualityLevel = 0.3, minDistance = 7, blockSize = 7 )
         kpt_aux = cv2.goodFeaturesToTrack(frame, mask = None, **feature_params)
         kpt = []
         for index in range(len(kpt_aux)):
@@ -76,6 +83,9 @@ def interest_point(frame, method='shiTomosi', harris_block_sz = 2, harris_apertu
         
     return kpt
 
+'''
+    Compute optical flow for each keypoints
+'''
 def parameters(curr_img, next_img, curr_kpts, neigh_size = 15, isfirst=True):
     Ix = derivate(curr_img, direct='X')
     Iy = derivate(curr_img, direct='Y')
@@ -108,7 +118,6 @@ def parameters(curr_img, next_img, curr_kpts, neigh_size = 15, isfirst=True):
                 S_Iyt = S_Iyt + It_ * Iy_ 
 
         X = np.array([[S_Ixx, S_Ixy], [S_Iyx, S_Iyy]])
-        #print('--> ', np.linalg.eig(X)[0])
         Y = -1 * np.array([[S_Ixt],[S_Iyt]])
         x_transpose = np.matrix.transpose(X)
         A = np.dot(x_transpose, X)
@@ -132,12 +141,16 @@ def validate_points(key_points,status,level=1):
         new_keypoints.append(np.array(aux))
         
     return np.array(new_keypoints)
-    
-def optical_flow(video, color, level=2,max_keypoints=100, file_name = 'dbg/flow_',kpts_method = 'sift',):
+   
+'''
+    Compute Optical Flow 
+''' 
+def optical_flow(video, color, level=2,max_keypoints=100, file_name = 'dbg/flow_',kpts_method = 'sift',neigh_size = 15):
     keypoints=[]
     pyramid = obtaining_pyramid(video,level)
     kpts = interest_point(pyramid[0], kpts_method)
-    print(len(kpts))
+    print("Nro. Keypoints : ",len(kpts))
+    print("Nro. Frames : ",len(video))
     kpts = kpts[0:min(max_keypoints,len(kpts)-1)]
     status=[True for i in range(len(kpts))]
     mask = np.zeros((video[0].shape[0], video[0].shape[1], 3))
@@ -147,9 +160,9 @@ def optical_flow(video, color, level=2,max_keypoints=100, file_name = 'dbg/flow_
         frame = cv2.cvtColor(frame, cv2.COLOR_GRAY2RGB)
         print("frame: ", index)
         if index==1:
-            flow = parameters(pyramid[index-1],pyramid[index],kpts, neigh_size = 15, isfirst=True)
+            flow = parameters(pyramid[index-1],pyramid[index],kpts, neigh_size = neigh_size, isfirst=True)
         else :
-            flow = parameters(pyramid[index-1],pyramid[index],kpts, neigh_size = 15, isfirst=False)
+            flow = parameters(pyramid[index-1],pyramid[index],kpts, neigh_size = neigh_size, isfirst=False)
             
         for i in range(0, min(max_keypoints,len(kpts))):
             if flow[i]!=[]:
@@ -170,7 +183,11 @@ def optical_flow(video, color, level=2,max_keypoints=100, file_name = 'dbg/flow_
         img = cv2.add(frame.astype('u1'),mask.astype('u1'))
         cv2.imwrite(file_name + str(index) + '.jpg', img)
     return validate_points(keypoints,status,level)
-    
+
+
+'''
+    Build the pyramid
+'''    
 def obtaining_pyramid(video,level=1):
     pyramid=[]
     kernel = np.ones((5,5),np.float32)/25
@@ -199,47 +216,56 @@ property uchar red
 end_header
 '''
 
-def write_ply(fn, verts, colors):
-    verts = verts.reshape(-1, 3)
-    colors = colors.reshape(-1, 3)
-    verts = np.hstack([verts, colors])
-    with open(fn, 'wb') as f:
-        f.write((ply_header % dict(vert_num=len(verts))).encode('utf-8'))
-        np.savetxt(f, verts, fmt='%f %f %f %d %d %d ')
-
+'''
+    Write rebuild points for meshlab
+'''
 def write_ply_h(fn, verts, colors, cams):
-    colors = colors.reshape(-1, 3)
+    colors = np.hstack((np.zeros((verts.shape[0],1)),np.full((verts.shape[0],1),255),np.zeros((verts.shape[0],1))))
     color_cams=np.hstack((np.zeros((cams.shape[0],1)),np.zeros((cams.shape[0],1)),np.full((cams.shape[0],1),255)))
     colors =np.vstack((colors,color_cams))
-    print(colors.shape,verts.shape,cams.shape)
     verts = np.vstack((verts, cams))
     verts = np.hstack([verts, colors])
     with open(fn, 'wb') as f:
         f.write((ply_header % dict(vert_num=len(verts))).encode('utf-8'))
         np.savetxt(f, verts, fmt='%f %f %f %d %d %d ')
 
+
+'''
+    Compute camera positions
+'''
+def compute_camera_position(M,F):
+    cams = []
+    factor = 500
+    for i in range(F):
+        cams.append(np.cross(M[i], M[i+F])*factor)
+    return np.array(cams)
+
+'''
+    Compute Structure From Motion
+'''    
 def structure_from_motion(kpts,color):
     P = len(kpts[0])
     F = len(kpts)
     #compute w
     X = []
     Y = []
-    for frame in kpts:
+    centroits=[]
+    for frame in kpts:         
         centroit = np.array([(float)(frame[:, 0].sum()), (float)(frame[:, 1].sum())])/ P
+        centroits.append(centroit)
         if X == []:
             X = np.array(frame[:, 0] - centroit[0])
             Y = np.array(frame[:, 1] - centroit[1])
         else:
             X = np.vstack((X, np.array(frame[:, 0] - centroit[0])))
             Y = np.vstack((Y, np.array(frame[:, 1] - centroit[1])))
+
     W = np.vstack((X, Y))
-    
     #apply SVD
     U, SIG, V_T = np.linalg.svd(W)
     U=U[:,:3]
     SIG=np.sqrt(np.diag(SIG[:3]))
-    V_T=V_T[:,:3]
-    V_T = np.matrix.transpose(V_T)
+    V_T=V_T[:3,:]
 
     M_hat = np.dot(U,SIG)
     S_hat = np.dot(SIG,V_T)
@@ -270,17 +296,20 @@ def structure_from_motion(kpts,color):
     M = np.dot(M_hat, A)
     S = np.dot(np.linalg.inv(A),S_hat)
     S = np.matrix.transpose(S)
+    cams = compute_camera_position(M,F)
+    write_ply_h("output/points.ply", S, color[:len(kpts[0])],cams)
 
-    write_ply("keypoints_origamy_17_all.ply", S[:,:3], color[:len(kpts[0])])
-
-def structure_from_motion_h(kpts,color):
+'''
+    Compute Structure From Motion with homogeneous coordinates
+'''  
+def structure_from_motion_homogenea(kpts,color):
     P = len(kpts[0])
     F = len(kpts)
     #compute w
     X = []
     Y = []
     for frame in kpts:
-        centroit = np.array([(float)(frame[:, 0].sum()), (float)(frame[:, 1].sum())])/ P
+        centroit = np.array([(float)(frame[:, 1].sum()), (float)(frame[:, 0].sum())])/ P
         if X == []:
             X = np.array(frame[:, 0] - centroit[0])
             Y = np.array(frame[:, 1] - centroit[1])
@@ -293,11 +322,10 @@ def structure_from_motion_h(kpts,color):
     U, SIG, V_T = np.linalg.svd(W)
     U=U[:,:4]
 
-    SIG=np.diag(SIG[:4])
-    V_T=V_T[:,:4]
-    V_T = np.matrix.transpose(V_T)
+    SIG=np.sqrt(np.diag(SIG[:4]))
+    V_T=V_T[:4,:]
 
-    M_hat = U
+    M_hat = np.dot(U,SIG)
     S_hat = np.dot(SIG,V_T)
     #compute A
     c = np.append(np.ones(2 * F), np.zeros(F))
@@ -340,24 +368,34 @@ def structure_from_motion_h(kpts,color):
         else:
             cams = np.vstack((cams, cam.ravel()))
     
-    write_ply_h("keypoints_chaous.ply", S[:,:3], color[:len(kpts[0])],cams)
+    write_ply_h("output/points2.ply", S[:,:3], color[:len(kpts[0])],cams)
 
-def load_images(file_):
-    video=[]
-    for i in range(9):
-        file_aux=file_+str(i+1)+".png"
-        img = cv2.imread(file_aux,0)
-        img = np.float32(img)
-        video.append(img)
-    return video
+def save_video(video, file_name = 'output/p2-1-1.avi', fps = 30, by_frame = False):
+    if by_frame:
+        cnt = 0
+        for frame in video:
+            cv2.imwrite((file_name + 'frame%d.jpg') % cnt, frame)
+            cnt += 1
+
+    else:
+        fourcc = cv2.VideoWriter_fourcc(*'DIVX')
+        out = cv2.VideoWriter(file_name, fourcc, fps, (video[0].shape[1], video[0].shape[0]))
+        for frame in video:
+            out.write(frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        out.release()
 if __name__ == '__main__':
-    file_name = 'input/video17.mp4'
-    neigh_size = 15
-    kpts_method_ = 'shiTomosi'
-    frame_per_sec = 30
-    color = np.random.randint(0,255,(1000,3))
-    video = load_video(file_name, frame_per_sec)
-    #video = load_images('input/dino000');    
-    keypoints = optical_flow(video,color, max_keypoints=1000,file_name = 'dbg/flow_17_all_', level=1,kpts_method = kpts_method_)
-    print("keypoints:  ",len(keypoints[0]),color[:len(keypoints)].shape)
+    file_name = 'input/p3-1-1.mp4'
+    video=[]
+    for i in range(129):
+        video.append(cv2.imread("dbg/flowSP_"+str(i+1)+".jpg"))
+    save_video(video)
+
+    neigh_size_ = 15
+    kpts_method_ = 'Harris'
+    frame_per_sec = 20
+    color = np.random.randint(0,255,(10000,3))
+    video = load_video(file_name, frame_per_sec)[0:300]
+    keypoints = optical_flow(video,color, max_keypoints=10000,file_name = 'output/flow_'+kpts_method_+'_', level=0,kpts_method = kpts_method_, neigh_size = neigh_size_)
     structure_from_motion(keypoints,color)
